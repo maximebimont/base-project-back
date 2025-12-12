2b3ebc26add4626db8b4a0759cbaf967a340fa3c
Maxime Bimont <mbperso2311@gmail.com>
2025-12-12 10:19:04 +0100

    first commit

---
+++ src/main/java/intellij/base_project_back/BaseProjectBackApplication.java
@@ -0,0 +1,13 @@
+package intellij.base_project_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseProjectBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/intellij/base_project_back/BaseProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package intellij.base_project_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
