5e119e1d907fdf999b902f885d6d3545336ee4ec
Maxime Bimont <mbperso2311@gmail.com>
2026-01-08 15:57:27 +0100

    test fix sonar

---
+++ src/test/java/intellij/base_project_back/BaseProjectBackApplicationTests.java
@@ -0,0 +1,12 @@
+package intellij.base_project_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
